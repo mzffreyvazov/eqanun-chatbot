8710f37131addf2bd4fb06c2dc4f9fa5f4657058
mzffreyvazov <mzffreyvazov2005@gmail.com>
2026-01-27 23:52:04 +0400

    refactor: integrate noStore for improved caching and clean up theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Toaster } from 'sonner';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
+import { unstable_noStore as noStore } from 'next/cache';
 
 import './globals.css';
 import { SessionProvider } from 'next-auth/react';
@@ -53,6 +54,7 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  noStore();
   return (
     <html
       lang="en"
